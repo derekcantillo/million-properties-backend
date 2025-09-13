e615a7240e30f4ad7eb43f319005085c025dbe97
derekcantillo <cantilloderek@gmail.com>
2025-09-13 09:25:49 -0500

    fix: build

--- obj/Debug/net9.0/MillionProperties.Api.AssemblyInfo.cs
+++ obj/Debug/net9.0/MillionProperties.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MillionProperties.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3c7856587384e4339b423a41d870726ca1c753f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d7182b64e87daf4e261eacab4d7b5ccf16e541e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MillionProperties.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MillionProperties.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
